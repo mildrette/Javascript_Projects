56605516425171cf73db577f4abb525c365eeae2
Mildred <99267408+mildrette@users.noreply.github.com>
2023-12-03 17:22:09 +0100

    javascript lesson day 1

--- JAVASCRIPT_Projects&Lessons/Script.js
+++ JAVASCRIPT_Projects&Lessons/Script.js
@@ -1,29 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const imageUpload = document.getElementById('image-upload');
-    const profileImage = document.getElementById('profile-image');
-    const useDefaultBtn = document.getElementById('use-default');
-  
-    // Handle file input change
-    imageUpload.addEventListener('change', function () {
-      const selectedFile = imageUpload.files[0];
-      if (selectedFile) {
-        // Display the selected image
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          profileImage.src = e.target.result;
-        };
-        reader.readAsDataURL(selectedFile);
-      }
-    });
-  
-    // Handle using default image
-    useDefaultBtn.addEventListener('click', function () {
-      profileImage.src = "https://img.freepik.com/free-vector/women-talking-concept-illustration_114360-8911.jpg?size=626&ext=jpg";
-      profileImage.src = "https://img.freepik.com/free-photo/woman-white-hoodie-with-yellow-background_1340-48101.jpg?size=626&ext=jpg" ;
-      profileImage.src = "https://img.freepik.com/free-vector/women-talking-concept-illustration_114360-8911.jpg?size=626&ext=jpg";
-      profileImage.src = "https://img.freepik.com/free-vector/women-talking-concept-illustration_114360-8911.jpg?size=626&ext=jpg";
+// Get a reference to the button element by its id
+const changeTextButton = document.getElementById('changeTextButton');
 
-      imageUpload.value ="https://img.freepik.com/free-photo/woman-white-hoodie-with-yellow-background_1340-48101.jpg?size=626&ext=jpg"; // Clear the selected image
-    });
-  });
-  
+// Add a click event listener to the button
+changeTextButton.addEventListener('click', function () {
+    // Change the text of the <h1> element
+    const header = document.querySelector('h1');
+    header.textContent = 'Text changed by clicking the button!';
+});
--- JAVASCRIPT_Projects&Lessons/new/Calculator.js
+++ JAVASCRIPT_Projects&Lessons/new/Calculator.js
@@ -1,28 +1,64 @@
-// JavaScript for the calculator
-document.addEventListener('DOMContentLoaded', function () {
-    const display = document.getElementById('display');
-    const buttons = document.querySelectorAll('input[type="button"]');
-    const clearButton = document.getElementById('clear');
-    const equalButton = document.getElementById('equal');
-  
-    // Add a click event listener to each button
-    buttons.forEach(function (button) {
-      button.addEventListener('click', function () {
-        if (this.value === '=') {
-          // Evaluate and display the result
-          try {
-            display.value = eval(display.value);
-          } catch (error) {
-            display.value = 'Error';
-          }
-        } else if (this.value === 'C') {
-          // Clear the display
-          display.value = '';
-        } else {
-          // Append the button value to the display
-          display.value += this.value;
+// Get references to the calculator elements
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.buttons button');
+
+let currentInput = '';
+let currentOperator = '';
+let firstOperand = null;
+
+// Function to update the display
+function updateDisplay() {
+    display.value = currentInput || '0';
+}
+
+// Add click event listeners to the buttons
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        const buttonValue = button.textContent;
+
+        if (buttonValue === 'C') {
+            // Clear the calculator
+            currentInput = '';
+            currentOperator = '';
+            firstOperand = null;
+        } else if (!isNaN(buttonValue) || buttonValue === '.') {
+            // Handle number input and decimal point
+            currentInput += buttonValue;
+        } else if ('+-*/'.includes(buttonValue)) {
+            // Handle operator input
+            if (currentOperator && currentInput) {
+                // Calculate the result when there's a previous operator and an input
+                firstOperand = calculate(firstOperand, currentInput, currentOperator);
+                currentInput = '';
+            }
+            currentOperator = buttonValue;
+        } else if (buttonValue === '=') {
+            // Calculate the result when the equals button is pressed
+            if (currentOperator && currentInput) {
+                firstOperand = calculate(firstOperand, currentInput, currentOperator);
+                currentInput = String(firstOperand);
+                currentOperator = '';
+            }
         }
-      });
+
+        updateDisplay();
     });
-  });
-  
+});
+
+// Function to perform calculations
+function calculate(firstOperand, secondOperand, operator) {
+    const num1 = parseFloat(firstOperand);
+    const num2 = parseFloat(secondOperand);
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        default:
+            return num2;
+    }
+}
