0ed009529eb0968b4f7993b28c99bac1454fc412
mildrette <mildredfonka1@gmail.com>
2024-11-03 23:38:45 +0100

    update to project

---
+++ Project.js
@@ -0,0 +1,4 @@
+console.log("Hello! i am leaarning Javascript. Again");
+
+let name = "mildred";
+let names;
---
+++ Variables.js
@@ -0,0 +1,6 @@
+let message = 'Mildred is learning Javascript, Again';
+
+let learning = 'javascript';
+let learningnext = 'java';
+let duration = 3;
+console.log(duration);
