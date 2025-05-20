4e1ce3afc7a71b9432b70281e44e21c87f8f2315
mildrette <mildredfonka1@gmail.com>
2025-05-20 12:35:12 +0100

    learning js all over again from the beginning

---
+++ day1.js
@@ -0,0 +1,17 @@
+
+let name;
+name = "mildred fonka ndadem sherlock";
+
+console.log(name);
+
+let message;
+message = "what do you think about this??"
+
+alert(message)
+
+let user = 'mildrd';
+let age = 30;
+let messages = 'learning new things';
+console.log(user, age, messages);
+
+// day one of learning js
