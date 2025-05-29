493b98be3e74f04a80e5741759721c0c94749000
mildrette <mildredfonka1@gmail.com>
2025-05-29 21:26:35 +0100

    conditional

---
+++ Conditional/index.js
@@ -0,0 +1,10 @@
+ let hours = 10;
+ 
+
+ if (hours >= 6 && hours < 12) {
+    console.log("Good morning my neghbor");
+ } else if (hours >= 1 && hours < 6) {
+    console.log("its afternoon, how do you do??")
+ } else {
+    console.log("good evening and good night")
+ }
